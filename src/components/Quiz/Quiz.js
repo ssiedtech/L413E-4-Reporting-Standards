8a4bf7d094da4d653293fd5858b0295d756cb1ea
Tanner <tanner.roof@outlook.com>
2021-04-01 14:51:11 -0400

    Updated meta data

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -2,57 +2,51 @@ export const quiz = {
   quizTitle: 'Check on Learning',
   questions: [
     {
-      question:
-        'The purpose of financial statements is to hold Government agencies accountable for their financial activities.',
+      question: "Which system produces the Army's financial statements?",
       questionType: 'text',
-      answers: ['True', 'False'],
-      correctAnswer: '1',
+      answers: ['DDRS-ARS', 'DDRS-AFS', 'GFRS-ARS', 'GFRS-AFS', 'GFRS-B'],
+      correctAnswer: '2',
       answerSelectionType: 'single',
     },
-
-    // ** Uncomment questions below to put them back in **
-
-    // {
-    //   question:
-    //     "What benefits does GFEBS bring to the Army's reporting practices?",
-    //   answers: [
-    //     'Real-time data',
-    //     'On Demand Reports',
-    //     'Customizable reports',
-    //     'All of the above',
-    //   ],
-    //   questionType: 'text',
-    //   correctAnswer: '4',
-    // },
-    // {
-    //   question:
-    //     'With ___________, financial managers can view up-to-date, accurate financial information about the Army at any point in time.',
-    //   answers: [
-    //     'Trial Balance',
-    //     'Customization',
-    //     'Real-time data',
-    //     'All of the above',
-    //   ],
-    //   correctAnswer: '3',
-    //   questionType: 'text',
-    // },
-    // {
-    //   question: "GFEBS produces the Army's financial statements.",
-    //   answers: ['True', 'False'],
-    //   correctAnswer: '2',
-    //   questionType: 'text',
-    // },
-    // {
-    //   question:
-    //     "What data does GFEBS send to DDRS so that DDRS can produce the Army's financial statements?",
-    //   answers: [
-    //     'Real-time data',
-    //     'Customizable reports',
-    //     'Trial Balance',
-    //     'All of the above',
-    //   ],
-    //   correctAnswer: '3',
-    //   questionType: 'text',
-    // },
+    {
+      question:
+        'Which of the following does DDRS do when it receives the GFEBS Trial Balance?',
+      answers: [
+        'Collects proprietary USSGL account balance information.',
+        "Produces the Treasury's government-wide consolidated financial statement.",
+        "Produces the Army's Financial Statements and Budget Execution Reports",
+        'Rejects it: DDRS does not accept Trial Balance data from GFEBS.',
+      ],
+      questionType: 'text',
+      correctAnswer: '3',
+    },
+    {
+      question: 'What is SFIS?',
+      answers: [
+        'Standard Financial Inquiry System.',
+        'DoD’s standard for categorizing financial information to support financial management and reporting functions.',
+        "Treasury's standard for importing a chart of accounts.",
+      ],
+      correctAnswer: '2',
+      questionType: 'text',
+    },
+    {
+      question:
+        'FACTS I, FACTS II and GFRS link the financial statements from the Army and other Federal Agencies to the Financial Report of the United States Government.',
+      answers: ['True', 'False'],
+      correctAnswer: '1',
+      questionType: 'text',
+    },
+    {
+      question: 'What is BEIS?',
+      answers: [
+        'A contractor company that supplies custom financials software.',
+        'A DoD organization that publishes the SFIS standards',
+        'An organization established by DoD to maintain SFIS and run DDRS.',
+        'A DoD organization that operates FACTS I and II.',
+      ],
+      correctAnswer: '3',
+      questionType: 'text',
+    },
   ],
 };
--- src/components/Slides/Slides.js
+++ src/components/Slides/Slides.js
@@ -4,6 +4,14 @@ import { AppContext } from '../../context/AppContext';
 import Quiz from 'react-quiz-component';
 import { quiz } from '../Quiz/Quiz';
 import TermsComponent from '../TermsComponent/TermsComponent.js';
+import GFEBS from '../../img/GFEBS.png';
+import { Image } from 'react-bootstrap';
+import Standards from '../../img/standards.jpg';
+import ReportingStandards from '../../img/reporting_standards.jpg';
+import GFRS from '../../img/gfrs.jpg';
+import FlowChart from '../../img/flowchart.png';
+import LearningCheckPoint from '../../img/module_checkpoint.png';
+import FMCrest from '../../img/fm_crest.png';
 
 function Slides() {
   // State management
@@ -61,6 +69,7 @@ function Slides() {
     autoplay: false,
     defaultIndex: 0,
     transitionDuration: 300,
+    canSwipe: false,
     prevArrow: (
       <div style={{ width: '30px', marginRight: '-30px' }}>
         <i className='fas fa-arrow-left'></i>
@@ -132,7 +141,9 @@ function Slides() {
                 </ul>
               </div>
               <div className='col'>
-                <span>IMAGE</span>
+                <span>
+                  <Image fluid className='mt-5 py-5' src={GFEBS} alt='' />
+                </span>
               </div>
             </div>
           </div>
@@ -164,7 +175,9 @@ function Slides() {
                   </ul>
                 </div>
               </div>
-              <div className='col'>IMAGE</div>
+              <div className='col'>
+                <Image fluid className='mt-5 py-5' src={GFEBS} alt='' />
+              </div>
             </div>
           </div>
           <div className='slide'>
@@ -181,99 +194,396 @@ function Slides() {
             <div className='row p-3 m-1'>
               <div className='col'>
                 <div>
-                  <h3 className='slide-title'>title</h3>
-                  <p>text</p>
+                  <h3 className='slide-title'>Overview</h3>
+                  <p>
+                    Congress, Treasury, and DoD all have reporting requirements
+                    and standards to which GFEBS must comply.
+                  </p>
+                  <p>This lesson will discuss:</p>
+                  <ul>
+                    <li>
+                      Reporting standards imposed by DoD, the Treasury, and
+                      Congress
+                    </li>
+                    <li>How GFEBS meets those standards</li>
+                    <li>
+                      External systems that require specific report formatting
+                      to enable GFEBS to transport data automatically
+                    </li>
+                  </ul>
                 </div>
               </div>
-              <div className='col'>IMAGE</div>
+              <div className='col'>
+                <Image fluid className='mt-5 py-5' src={GFEBS} alt='' />
+              </div>
             </div>
           </div>
           <div className='slide'>
             <div className='row p-3 m-1'>
               <div className='col'>
                 <div>
-                  <h3 className='slide-title'>title</h3>
-                  <p>text</p>
+                  <h3 className='slide-title'>Standardize Reporting</h3>
+                  <p>
+                    Standardizing reporting practices throughout DoD and
+                    Treasury has several benefits. The following benefits
+                    support transparency and affords organization.
+                  </p>
+                  <ul>
+                    <li>
+                      Enables decision-makers to efficiently compare similar
+                      programs and activities throughout DoD.
+                    </li>
+                    <li>
+                      Provides decision-makers the level of detail they require
+                      to retrieve information and perform audits.
+                    </li>
+                    <li>
+                      Improves the efficiency of maintaining business systems,
+                      which reduces the costly maintenance and translation of
+                      nonstandard data.
+                    </li>
+                    <li>
+                      Links program management to its budgetary resources and
+                      its actual financial information, which consequently
+                      improves performance.
+                    </li>
+                  </ul>
                 </div>
               </div>
-              <div className='col'>IMAGE</div>
             </div>
           </div>
           <div className='slide'>
             <div className='row p-3 m-1'>
               <div className='col'>
                 <div>
-                  <h3 className='slide-title'>title</h3>
-                  <p>text</p>
+                  <h3 className='slide-title'>
+                    Department of Defense (DoD) Reporting Standards
+                  </h3>
+                  <p>
+                    <b>Standard Financial Information Structure (SFIS)</b>
+                    <br />
+                    Standard Financial Information Structure (SFIS) is a
+                    standard for categorizing financial information to support
+                    financial management and reporting functions. It provides a
+                    common foundation to track, process, and report DoD business
+                    transactions. It also plays a key role in helping achieve
+                    and sustain an unqualified audit opinion on consolidated DoD
+                    financial statements. SFIS supports requirements for
+                    budgeting, performance-based management, and the generation
+                    of financial statements.
+                  </p>
                 </div>
               </div>
-              <div className='col'>IMAGE</div>
+              <div className='col'>
+                <Image fluid className='mt-5 py-5' src={GFEBS} alt='' />
+              </div>
             </div>
           </div>
           <div className='slide'>
             <div className='row p-3 m-1'>
               <div className='col'>
                 <div>
-                  <h3 className='slide-title'>title</h3>
-                  <p>text</p>
+                  <h3 className='slide-title'>SFIS Standards (1 of 2)</h3>
+                  <p>
+                    <b>Who maintains the SFIS standards?</b>
+                  </p>
+                  <p>
+                    BEIS is the organization established by DoD responsible for
+                    maintaining SFIS and running the DDRS.
+                  </p>
+                  <p>BEIS performs the following specific functions:</p>
+                  <ul>
+                    <li>
+                      Collects financial transactions from throughout DoD,
+                      through DDRS.
+                    </li>
+                    <li>Provides the authoritative source for SFIS values.</li>
+                    <li>Ensures that data is compliant with SFIS standards.</li>
+                    <li>
+                      Provides security-defined, enterprise-level access to
+                      information for ad-hoc management queries.
+                    </li>
+                    <li>
+                      Produces external financial management reports/statements
+                      based on standardized data.
+                    </li>
+                  </ul>
                 </div>
               </div>
-              <div className='col'>IMAGE</div>
+              <div className='col'>
+                <Image fluid className='mt-5 py-5' src={GFEBS} alt='' />
+              </div>
             </div>
           </div>
           <div className='slide'>
             <div className='row p-3 m-1'>
               <div className='col'>
                 <div>
-                  <h3 className='slide-title'>title</h3>
-                  <p>text</p>
+                  <h3 className='slide-title'>SFIS Standards (2 of 2)</h3>
+
+                  <p>
+                    At the end of each period, GFEBS sends its Trial Balance to
+                    the DDRS, which requires GFEBS to format its Trial Balance
+                    according to SFIS standards.
+                  </p>
+                  <ul>
+                    <li>
+                      The GFEBS Trial Balance includes the Defense Reporting
+                      Elements (DRE), a subset of SFIS.
+                    </li>
+                    <li>
+                      Each point account in GFEBS links to the appropriate SFIS
+                      attribute and to the DoD reporting Chart of Accounts
+                      number.
+                    </li>
+                    <li>
+                      DDRS uses the GFEBS Trial Balance to produce all budget
+                      and execution reports and financial statements for the
+                      Army:
+                    </li>
+                    <ul>
+                      <li>DDRS-B produces budgetary reports</li>
+                      <li>DDRS-AFS produces financial statements</li>
+                    </ul>
+                    <li>
+                      DDRS consolidates this with the trial balances from all
+                      other DoD organizations to produce a unified DoD Trial
+                      Balance for Congress, the Treasury, and the public.
+                    </li>
+                  </ul>
                 </div>
               </div>
-              <div className='col'>IMAGE</div>
+              <div className='col'>
+                <Image
+                  fluid
+                  className=' py-5'
+                  src={Standards}
+                  alt='Standards'
+                />
+              </div>
+            </div>
+          </div>
+          <div className='slide'>
+            <div className='row p-3 m-1'>
+              <div className='col-7'>
+                <div>
+                  <h3 className='slide-title'>
+                    Department of the Treasury Reporting Standards
+                  </h3>
+                  <p>
+                    The Federal Agencies' Centralized Trial Balance System
+                    (FACTS) is administered and maintained by the Department of
+                    the Treasury's Financial Management Service (FMS) to prepare
+                    the Financial Report of the United States Government at
+                    year-end. FACTS I collects proprietary USSGL account balance
+                    information at the end of each fiscal year.
+                  </p>
+                  <p>
+                    FACTS II is also administered by Treasury FMS. It collects
+                    budget execution data at the end of each fiscal quarter.
+                    FACTS I and FACTS II exist simultaneously and run
+                    independently of each other.
+                  </p>
+                  <p>
+                    To enable DoD to compile a Trial Balance that meets Treasury
+                    Standards, the Trial Balance that GFEBS sends to DDRS
+                    complies with FACTS I and II reporting standards. To achieve
+                    this, GFEBS maintains an association between its Chart of
+                    Accounts and the attribute values used by FACTS I and FACTS
+                    II.
+                  </p>
+                </div>
+              </div>
+              <div className='col'>
+                <Image
+                  fluid
+                  className='mt-5 py-5'
+                  src={ReportingStandards}
+                  alt='Reporting Standards'
+                />
+              </div>
             </div>
           </div>
           <div className='slide'>
             <div className='row p-3 m-1'>
               <div className='col'>
                 <div>
-                  <h3 className='slide-title'>title</h3>
-                  <p>text</p>
+                  <h3 className='slide-title'>GFRS</h3>
+                  <p>
+                    <b>Government-Wide Financial Reporting System (GFRS)</b>
+                  </p>
+                  <p>
+                    GFEBS also maintains an association between its Chart of
+                    Accounts and attribute values used by GFRS. GFRS is
+                    maintained by Treasury.
+                  </p>
+                  <p>
+                    It captures each Federal agency's closing information and
+                    links the agencies' audited and consolidated
+                    department-level financial statements to the Financial
+                    Report of the United States Government. GFRS resolves any
+                    deficiencies identified by the Government Accountability
+                    Office (GAO). Although DoD sends the consolidated financial
+                    statement to GFRS, GFEBS formats its Trial Balance according
+                    to GFRS so that DoD does not need to restructure data to
+                    make it GFRS-compatible.
+                  </p>
                 </div>
               </div>
-              <div className='col'>IMAGE</div>
+              <div className='col'>
+                <Image fluid className='p-5' src={GFRS} alt='GFRS' />
+              </div>
             </div>
           </div>
           <div className='slide'>
             <div className='row p-3 m-1'>
               <div className='col'>
                 <div>
-                  <h3 className='slide-title'>title</h3>
-                  <p>text</p>
+                  <h3 className='slide-title'>External Data Flows (1 of 2)</h3>
+                  <p>
+                    <b>External Reporting Data Flows</b>
+                  </p>
+                  <p>
+                    <b>GFEBS</b>
+                    <br />
+                    GFEBS starts with consolidated financial data for the
+                    previous fiscal period, as posted to the G/L.
+                  </p>
+                  <p>
+                    <b>GFEBS</b>
+                    <br />
+                    FI runs the DDRS Trial Balance and sends it to the DDRS. The
+                    Trial Balance Data Structure complies with SFIS, FACTS I,
+                    FACTS II, and GFRS reporting requirements.
+                  </p>
+                  <p>
+                    <b>DDRS</b>
+                    <br />
+                    DDRS, a system managed by DoD's BEIS produces the financial
+                    statements for the Army. DDRS then consolidates all DoD
+                    financial statements and produces the official DoD Financial
+                    Statement.
+                  </p>
                 </div>
               </div>
-              <div className='col'>IMAGE</div>
+              <div className=''>
+                {' '}
+                <Image
+                  fluid
+                  className='mt-3 my-auto col-7'
+                  src={FlowChart}
+                  alt='Flow Chart'
+                />
+              </div>
+            </div>
+          </div>
+          <div className='slide'>
+            <div className='row p-3 m-1'>
+              <div className='col-7'>
+                <div>
+                  <h3 className='slide-title'>External Data Flows (2 of 2)</h3>
+                  <p>
+                    <b>External Reporting Data Flows</b>
+                  </p>
+                  <p>
+                    <b>US Treasury</b>
+                    <br />
+                    The U.S. Treasury uses FACTS I, FACTS II, and GFRS to
+                    produce the Financial Report for the United States
+                    Government.
+                  </p>
+                  <p>
+                    <b>Office of Management and Budget (OMB)</b>
+                    <br />
+                    The OMB receives the DoD Financial Statement, which helps it
+                    hold the DoD accountable for its financial activities.
+                  </p>
+                  <p>
+                    <b>
+                      U.S. Congress | Government Accountability Office (GAO) |
+                      American Public
+                    </b>
+                    <br />
+                    The financial statements from the DoD and other Federal
+                    agencies, the U.S. Treasury's Financial Report, and reports
+                    from the OMB are provided to the U.S. Congress, the GAO, and
+                    to the public. The availability and visibility of financial
+                    activities promotes more fiscal responsibility within each
+                    Federal agency.
+                  </p>
+                </div>
+              </div>
+              <div className='my-auto col'>
+                {' '}
+                <br />
+                <Image fluid className='' src={FlowChart} alt='Flow Chart' />
+              </div>
             </div>
           </div>
           <div className='slide'>
             <div className='row p-3 m-1'>
               <div className='col'>
                 <div>
-                  <h3 className='slide-title'>title</h3>
-                  <p>text</p>
+                  <h3 className='slide-title'>Learning Checkpoint</h3>
+                  <p>
+                    The following exercise consists of 6 questions to test your
+                    comprehension of the previous information presented.
+                  </p>
                 </div>
               </div>
-              <div className='col'>IMAGE</div>
+              <div className='col'>
+                <Image
+                  className='m-3'
+                  fluid
+                  src={LearningCheckPoint}
+                  alt='Learning Checkpoint'
+                />
+              </div>
+            </div>
+          </div>
+          <div className='slide'>
+            <div className='row p-3 m-1'>
+              <div className='col'>
+                <Quiz
+                  quiz={quiz}
+                  key={key}
+                  continueTillCorrect={true}
+                  showDefaultResult={false}
+                  onComplete={onCompleteAction}
+                  customResultPage={renderCustomResultPage}
+                />
+              </div>
             </div>
           </div>
           <div className='slide'>
             <div className='row p-3 m-1'>
               <div className='col'>
                 <div>
-                  <h3 className='slide-title'>title</h3>
-                  <p>text</p>
+                  <h3 className='slide-title'>Summary</h3>
+                  <p>In this module you have learned to:</p>
+
+                  <ul>
+                    <li>Define SFIS and explain its purpose.</li>
+                    <li>Describe how GFEBS achieves SFIS compliance.</li>
+                    <li>Define DDRS and explain why GFEBS must use it.</li>
+                    <li>
+                      Define BEIS and explain its relationship to DDRS and
+                      GFEBS.
+                    </li>
+                    <li>
+                      Explain the importance of FACTS I and FACT II to GFEBS.
+                    </li>
+                    <li>
+                      Define GFRS, explain its purpose, and how it is associated
+                      with GFEBS.
+                    </li>
+                  </ul>
                 </div>
               </div>
-              <div className='col'>IMAGE</div>
+              <div className='col'>
+                {' '}
+                <Image className='p-5' fluid src={FMCrest} alt='FM Crest' />
+              </div>
             </div>
           </div>
         </Slide>
